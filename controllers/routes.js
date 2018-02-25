5bef25a0262d73d45069c4ec0c23a298c845ba74
PlugInPlaya <tsanders1114@gmail.com>
2018-02-25 14:49:01 -0500

    Changed View files around

--- controllers/routes.js
+++ controllers/routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res){
-    res.render('./views/layouts/index');
+    res.render('index');
 });
 
 module.exports = router;
